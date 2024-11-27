985e56e8ce7e10fa2967d56c2321cd633d5ac60b
Echo-Minecraft-Team <itsnotalexthere@gmail.com>
2024-11-27 22:23:15 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         My posts
       </h1>
       <p className="mb-4">
-        {`I'm a small dev, working on a project called Echo.`}
+        {`I'm a small dev, working on a project called Echo. This blog documents my journey, including technical challenges, progress updates, and insights into creating innovative solutions in the server hosting space. It also includes personal articles.`}
       </p>
       <div className="my-8">
         <BlogPosts />
