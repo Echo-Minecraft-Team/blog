ddc126bc2e940e3bfc48c65e4ab02ad96907e5f2
Echo-Minecraft-Team <itsnotalexthere@gmail.com>
2024-11-27 21:40:18 +0100

    Update and remove the vercel template

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Echo Updates
       </h1>
       <p className="mb-4">
-        {`I'm a small dev, working on a.`}
+        {`I'm a small dev, working on a project called Echo.`}
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
